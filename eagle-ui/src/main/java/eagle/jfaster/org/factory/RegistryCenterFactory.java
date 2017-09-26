091e1b8db803a5d891affa57beca5b13b65a0dc7
fangyanpeng <13120336627@163.com>
2017-09-26 16:50:09 +0800

    license

--- eagle-ui/src/main/java/eagle/jfaster/org/factory/RegistryCenterFactory.java
+++ eagle-ui/src/main/java/eagle/jfaster/org/factory/RegistryCenterFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2015 dangdang.com.
+ * Copyright 2017 eagle.jfaster.org.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
